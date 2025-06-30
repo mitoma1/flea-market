3c9a3dc7764ba6a96c6c4f86c1c1d6e0885d8d04
mitoma１　 <pasako.0617@icloud.com>
2025-06-30 16:13:45 +0900

    リモートリポジトリの変更

--- src/database/seeders/ProductSeeder.php
+++ src/database/seeders/ProductSeeder.php
@@ -4,13 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use Faker\Factory as Faker;
 
 class ProductSeeder extends Seeder
 {
     public function run()
     {
+        $faker = Faker::create();
+        $userIds = User::pluck('id')->toArray(); // 既存ユーザーIDを取得
+
         DB::table('products')->insert([
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => '腕時計',
                 'image' => 'watch.jpg',
                 'price' => 15000,
@@ -18,6 +24,7 @@ class ProductSeeder extends Seeder
                 'description' => 'スタイリッシュなデザインのメンズ腕時計',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'HDD',
                 'image' => 'hdd.jpg',
                 'price' => 5000,
@@ -25,6 +32,7 @@ class ProductSeeder extends Seeder
                 'description' => '高速で信頼性の高いハードディスク',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => '玉ねぎ3束',
                 'image' => 'onion.jpg',
                 'price' => 300,
@@ -32,6 +40,7 @@ class ProductSeeder extends Seeder
                 'description' => '新鮮な玉ねぎ３玉のセット',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => '革靴',
                 'image' => 'shoes.jpg',
                 'price' => 4000,
@@ -39,6 +48,7 @@ class ProductSeeder extends Seeder
                 'description' => 'クラシックなデザインの革靴',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'ノートPC',
                 'image' => 'laptop.jpg',
                 'price' => 45000,
@@ -46,6 +56,7 @@ class ProductSeeder extends Seeder
                 'description' => '高性能なノートパソコン',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'マイク',
                 'image' => 'microphone.jpg',
                 'price' => 8000,
@@ -53,6 +64,7 @@ class ProductSeeder extends Seeder
                 'description' => '高音質のレコーディング用マイク',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'ショルダーバッグ',
                 'image' => 'shoulder_bag.jpg',
                 'price' => 3500,
@@ -60,6 +72,7 @@ class ProductSeeder extends Seeder
                 'description' => 'おしゃれなショルダーバッグ',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'タンブラー',
                 'image' => 'tumbler.jpg',
                 'price' => 500,
@@ -67,6 +80,7 @@ class ProductSeeder extends Seeder
                 'description' => '使いやすいタンブラー',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'コーヒーミル',
                 'image' => 'coffee_mill.jpg',
                 'price' => 4000,
@@ -74,6 +88,7 @@ class ProductSeeder extends Seeder
                 'description' => '手動のコーヒーミル',
             ],
             [
+                'user_id' => $faker->randomElement($userIds),
                 'name' => 'メイクセット',
                 'image' => 'makeup_set.jpg',
                 'price' => 2500,
